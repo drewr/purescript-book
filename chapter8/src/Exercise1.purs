cf2023a332144d2c24f35076688e46d2ad20541d
Drew Raines <drew@raines.me>
2016-08-02 11:01:12 -0500

    Save ch8ex01

--- chapter8/src/Exercise1.purs
+++ chapter8/src/Exercise1.purs
@@ -10,3 +10,26 @@ third xs = do
   zs <- tail ys
   z <- head zs
   pure z
+
+-- > :r
+-- Compiling Exercise1
+-- > import Prelude
+-- > import Data.Maybe
+-- > Just 1
+-- (Just 1)
+
+-- > :t (<*>)
+-- forall a b f. (Apply f) => f (a -> b) -> f a -> f b
+
+-- > :t apply
+-- forall a b f. (Apply f) => f (a -> b) -> f a -> f b
+
+-- > :t ap
+-- forall m a b. (Monad m) => m (a -> b) -> m a -> m b
+
+-- > ap (Just (\x -> x + 1)) (Just 1)
+-- (Just 2)
+
+-- > apply (Just (\x -> x + 1)) (Just 1)
+-- (Just 2)
+
